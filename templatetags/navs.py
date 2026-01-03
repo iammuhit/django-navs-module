73aefe19f8462e603a5b2804072f368099789d2e
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2026-01-03 21:13:47 +0100

    create menu and links template-tags

---
+++ templatetags/navs.py
@@ -0,0 +1,21 @@
+from django import template
+
+from app.modules.navs.models import Menu
+
+register = template.Library()
+
+
+@register.inclusion_tag('navs/menu.html', takes_context=True)
+def nav_menu(context, slug):
+    try:
+        menu  = Menu.objects.get(slug=slug, is_active=True)
+        links = menu.links.filter(parent__isnull=True, is_active=True)
+    except Menu.DoesNotExist:
+        return {'links': []}
+    
+    return {'links': links, 'request': context.get('request')}
+
+
+@register.inclusion_tag('navs/links.html', takes_context=True)
+def nav_links(context, links):
+    return {'links': links, 'request': context.get('request')}
