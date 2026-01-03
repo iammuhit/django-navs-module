c5d7c7f5a43c5f1410d99673f96474bd8ac5885f
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2026-01-03 21:12:46 +0100

    register menu and link admin

--- admin.py
+++ admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.modules.navs.models import Link, Menu
+
+
+class LinkInline(admin.TabularInline):
+    model = Link
+    extra = 1
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'is_active')
+    prepopulated_fields = {'slug': ['name']}
+    inlines = [LinkInline]
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'menu', 'parent', 'order', 'is_active')
+    list_filter = ('menu', 'is_active')
+    search_fields = ('title',)
